42a652be7d54e7cb5280900f80ec6a8d328cff7d
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-02-26 22:41:05 -0800

    Update 155. Min Stack.py

--- Neetcode/Stack/155. Min Stack.py
+++ Neetcode/Stack/155. Min Stack.py
@@ -1,29 +1,33 @@
-class MinStack:
+# OPTIMAL Solution
 
+class MinStack:
     def __init__(self):
         self.stack = []
         self.minStack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        if not self.minStack or val <= self.minStack[-1]:
-            self.minStack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
 
     def pop(self) -> None:
-        if self.stack:
-            if self.stack[-1] == self.minStack[-1]:
-                self.minStack.pop()
-            self.stack.pop()
-        
+        self.stack.pop()
+        self.minStack.pop()
 
     def top(self) -> int:
-        if self.stack:
-            return self.stack[-1]
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        if self.minStack:
-            return self.minStack[-1]
+        return self.minStack[-1]
 
 '''
 Explained: Time Complexity: O(1) Space Complexity: O(n)
+We know that a regular stack the time complexity for push, pop and top is O(1). But to
+make getMin O(1). We have a seperate stack that tracks the minimum number at each level of the
+original stack. Basically we have two stacks. But the minStack's main goal is to track the minimum
+value at that level. So when appending a value we first append the value to regular stack then
+we check if the current value is smaller than the current top of our minstack and also make sure
+that it is not empty and hold the result and then append that result into the minstack. For pop we
+pop both stacks at the same time. For getting the top of a stack we do it for regular stack. For getMin
+we just return whatever is on the top of the minStack. Thus making it easier.
 '''
