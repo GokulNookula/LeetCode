b01dd237028344ef5202090346e0e36242d3c8bb
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-03-24 22:31:00 -0700

    Update 15. 3Sum.py

--- Neetcode/Two Pointers/15. 3Sum.py
+++ Neetcode/Two Pointers/15. 3Sum.py
@@ -120,3 +120,34 @@ class Solution:
                     while l < r and nums[l] == nums[l - 1]:
                         l += 1
         return res
+
+# My solution - OPTIMAL on 3/24/2025
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        for i in range(len(nums)):
+
+            # To find the duplicate
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                total  = nums[i] + nums[l] + nums[r]
+
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+
+                    l += 1
+                    r -= 1
+
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+        return res
