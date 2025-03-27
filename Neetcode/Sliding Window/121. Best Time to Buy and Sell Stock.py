5f453805612c6abdc9f4720ea5f1522ed62833ed
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-03-26 22:52:42 -0700

    Update 121. Best Time to Buy and Sell Stock.py

--- Neetcode/Sliding Window/121. Best Time to Buy and Sell Stock.py
+++ Neetcode/Sliding Window/121. Best Time to Buy and Sell Stock.py
@@ -30,21 +30,33 @@ class Solution:
         return maxProfit
 
 '''Explained: Time Complexity: O(n) Space Complexity: O(1)
-
+We know that time only moves with the days increasing thus by this we should know that we need to use two pointers.
+Thus we first set left pointer as 0th element aka day 1 in the array and right to the day 2 of the array. Now we
+keep iterating over the array while our right pointer is less than the length of the price array, aka the end of trading
+days data. For the if statement we know that we only go into it if we buy low aka left is less and we sell high aka
+right is greater than left. So when we go in we need to calculate the profit for those days. Next once we do we need to
+make sure that if our current profit we calculated is greater than our maxProfit. If it is then we update it or else 
+we just move our right pointer by +1. Else if we are buying high or buying at the same price as sell price being low or
+equal to buy price then we update our left pointer to our current right pointer as we want to buy at the lowest price
+and then we increase the right pointer by +1. Thus by handling it this way we are only iterating over the array once
+and we do not use any arrays to store the profit calculated thus making it O(1).
 '''
 
 
-# My Solution - Not Optimal
+# My Solution with AI - OPTIMAL on 3/26/2025
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
         minPrice = prices[0]
         maxProfit = 0
 
-        for price in prices:
+        for sell in prices:
             minPrice = min(minPrice, price)
-            profit = price - minPrice
+            profit = sell - minPrice
             maxProfit = max(maxProfit, profit)
         
         return maxProfit
-
+'''Explained: Time Complexity: O(n) Space Complexity: O(1)
+This uses dynamic programming to also calculate the profit while focusing on finding the minimum price for buying 
+stored while we iterate over for all the sell prices in the array and then only store the one that gives us the most profit
+'''
