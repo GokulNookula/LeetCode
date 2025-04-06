be408e49fc066cadf5ae6c6037fa055dbc708d5a
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-04-06 12:14:57 -0700

    Update 424. Longest Repeating Character Replacement.py

--- Neetcode/Sliding Window/424. Longest Repeating Character Replacement.py
+++ Neetcode/Sliding Window/424. Longest Repeating Character Replacement.py
@@ -1,20 +1,42 @@
 # OPTIMAL Solution - beacause it is isn't O(26 * n) instead it is O(n)
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        # Hashmap to access each character count from string fast
         count = {}
+        # The longest repeating character answer stored
         res = 0
         
+        # Beginning of the left pointer index
         l = 0
+        # To keep track of the longest character sequence 
         maxf = 0
+        # Second pointer increases it's size as we iterate over the
+        # entire length of the string to track the longest charcter sequence
         for r in range(len(s)):
+            # To initalize the character in the dictonary whether if it exists
+            # then we just find the current count from dictonary and increase it
+            # by 1 or else we initalize it with count.get to 0 and increase it by 1
             count[s[r]] = 1 + count.get(s[r], 0)
+            # To keep track of the longest character count to save time in checking
+            # for the while loop when removing characters if we exceed k replacement
             maxf = max(maxf, count[s[r]])
 
+            # While windowlength aka (r - l + 1) minus the maximum character in
+            # our dictonary of count to get the number of characters count that
+            # are not the same character as our maximum character and then we
+            # check the number of those characters is less than allowed replacable
+            # count given to us aka (windowlen - maxCharacterFreq) > k
             while (r - l + 1) - maxf > k:
+                # We decrement the count of that variable from the left as
+                # we move our left pointer by 1 to remove it from our dictonary
                 count[s[l]] -= 1
+                # Moving the left pointer by 1 until we meet the above condition
                 l += 1
+            # Returning the maximum character length with the replacable characters
+            # being replaced already
             res = max(res, r - l + 1)
 
+        # Returning the maximum character
         return res
 
 '''Explained: Time Complexity: O(n) Spac Complexity: O(n)
