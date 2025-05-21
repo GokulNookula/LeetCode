dc88cd89764ef132a06f02de2853a4ae977fde5c
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-05-20 22:21:12 -0700

    Create 76. Minimum Window Substring.py

---
+++ Neetcode/Sliding Window/76. Minimum Window Substring.py
@@ -0,0 +1,67 @@
+# OPTIMAL Solution
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        # Testcase when if t is empty string
+        if t == "":
+            return ""
+
+        # Setting up the hashmaps for countT and current window
+        countT = {}
+        window = {}
+
+        # Initalizing the countT hashmap as it does not change
+        for c in t:
+            # Basically finding the value in hashmap if found increase it by 1
+            # or else we default the value to 0 by .get and increment it by 1 aka
+            # set it to 1
+            countT[c] = 1 + countT.get(c, 0)
+        # This is for storing how many matches we found from s and t
+        have = 0
+        # The total characters in the string t that we must match for s
+        need = len(countT)
+        # Storing the left and right pointer of the substring we found
+        res = [-1, -1]
+        # Storing the smallest substring length and updating if we dont find it
+        resLen = float("infinity")
+        l = 0
+        # Iterating through every character in s
+        for r in range(len(s)):
+            # Getting aka storing the current character we just reached
+            c = s[r]
+            # Basically finding the value in hashmap if found increase it by 1
+            # or else we default the value to 0 by .get and increment it by 1 aka
+            # set it to 1
+            window[c] = 1 + window.get(c, 0)
+
+            # Check if we satisfied the count window for the first time or not
+            # and we check if c character is in countT
+            if ((c in countT) and (window[c] == countT[c])):
+                # Increment the have as we met the condition
+                have += 1
+            # We try reduce the window size if we met the condition
+            while have == need:
+                # Checking if our window is smaller than the previous one
+                if (r - l + 1) < resLen:
+                    # Updating our result
+                    res = [l, r]
+                    resLen = (r - l + 1)
+                # We need to pop from left until we dont meet the have == need
+                # Thus to find the smallest substring of t in s
+                window[s[l]] -= 1
+                # Checking if decrementing it caused the have to go down
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    # If it did then we reduce it
+                    have -= 1
+                l += 1
+        # Extracting the left and right pointer from the result array to check stuff
+        # in the return function
+        l, r = res
+        # We do s[l: r + 1] + 1 for off by 1 adjustment and we do this to check
+        # if resLen has been updated aka case where if a result does not exist 
+        # then we return an empty string or else we return s[l: r + 1]
+        return s[l : r + 1] if resLen != float("infinity") else ""
+
+'''Explained: Time Complexity: O(n) Space Complexity: O(m)
+'''
