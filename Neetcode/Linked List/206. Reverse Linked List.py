14aca41bc8b848120d8cafa33cc82d7f59ec654c
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-06-22 22:35:39 -0700

    Update 206. Reverse Linked List.py

--- Neetcode/Linked List/206. Reverse Linked List.py
+++ Neetcode/Linked List/206. Reverse Linked List.py
@@ -28,3 +28,21 @@ class Solution:
 
 '''Explained Time Complexity: O(n) and Space Complexity: O(1)
 '''
+
+# My solution - OPTIMAL on 6/22/2025
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
