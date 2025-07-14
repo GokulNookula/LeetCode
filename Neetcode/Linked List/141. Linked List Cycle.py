852950905e952cca77391bf9e8fe9f7e9eb2d6ce
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-13 17:33:39 -0700

    Update 141. Linked List Cycle.py

--- Neetcode/Linked List/141. Linked List Cycle.py
+++ Neetcode/Linked List/141. Linked List Cycle.py
@@ -26,6 +26,14 @@ class Solution:
         # If we don't see a cycle we return False
         return False
 '''Explained: Time Complexity: O(n) and Space Complexity: O(1)
+In this problem we use the solution of Floyd's cycle algorithm aka Hare and Tortoise algorithm
+where we initalize two pointers called slow and fast and next in the while loop we check if
+our current fast pointer is pointing to null pointer and if fast.next is pointing to null pointer
+which means we are at the end of the list and that we did not find a cycle this ending the algorithm.
+Next if there is a cycle we move slow pointer by 1 node and fast pointer by 2 pointers and according to
+Floyd's algorithm no matter what these pointers will always meet sometime. Thus we iterate and if 
+slow and fast pointer are pointing to the same point then there is a cycle thus we return True or else
+we exit our while loop and return False.
 '''
 
 # My solution - not optimal on 6/19/2025
@@ -45,4 +53,25 @@ class Solution:
             else:
                 return True
             head = head.next
+        return False
+
+# My solution - OPTIMAL on 7/13/2025
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
         return False
