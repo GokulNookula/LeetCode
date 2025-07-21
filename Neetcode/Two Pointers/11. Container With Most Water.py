e289343e418d19e314a451776058f3eaf8a7a93a
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-20 19:33:11 -0700

    Update 11. Container With Most Water.py

--- Neetcode/Two Pointers/11. Container With Most Water.py
+++ Neetcode/Two Pointers/11. Container With Most Water.py
@@ -89,7 +89,7 @@ class Solution:
                 left += 1
         return maxArea
 
-# My solution - OPTIMAL on 7/20/2025
+# My solution - OPTIMAL on 6/20/2025
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         left = 0
@@ -109,3 +109,33 @@ class Solution:
             else:
                 right -= 1
         return maxArea
+
+# My solution - OPTIMAL on 7/20/2025
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        for i in range(len(nums)):
+            
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left = i + 1
+            right = len(nums) - 1
+    
+            while (left < right):
+                total = nums[i] + nums[left] + nums[right]
+
+                if total > 0:
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    res.append([nums[i],nums[left],nums[right]])
+                    left += 1
+                    right -= 1
+
+                    while (left < right) and nums[left] == nums[left - 1]:
+                        left += 1
+
+        return res
