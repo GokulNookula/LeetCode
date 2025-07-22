d2b9c215a6299ff5203ebedd51f607fa4e2fb36b
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-21 23:10:09 -0700

    Update 226. Invert Binary Tree.py

--- Neetcode/Trees/226. Invert Binary Tree.py
+++ Neetcode/Trees/226. Invert Binary Tree.py
@@ -5,10 +5,44 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         
-        # Using DFS Base case where we reach the next node of
+        # We use a Pre-Order DFS where we initalize 
+        # the base case when we reach the root we stop
+        # the recursion
+        if not root:
+            return None
+
+        # We swap nodes first then go down
+        # Storing the left node to swap nodes
+        temp = root.left
+        # Assigning left node to the right node
+        root.left = root.right
+        # Assigning right node to the left node temp val
+        root.right = temp
+        
+        # First iteration we swap the left and right node 
+        # of the root then we go to the deepest left
+        # node and once we hit the child we recursively
+        # loop back and swap the left most tree node first
+        # then go to the right most tree node and swap those
+        # values
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        # Returning the root value to get the inverted tree
+        return root
+        
+  '''Explained: Time Complexity: O(n) and Space Complexity: O(n) due to recursive stack
+  '''
+
+# My solution - Didnt Solve it myself on 7/21/2025
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        # Using DFS - POST-order Base case where we reach the next node of
         # the child node is none
         if root is None:
             return None
@@ -29,6 +63,3 @@ class Solution:
         # Returning after we reach to back to the top
         # aka the root
         return root
-
-  '''Explained: Time Complexity: O(n) and Space Complexity: O(n) due to recursive stack
-  '''
