ed5eb3fef37558ad030203300200e9dd5056149c
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-26 20:34:01 -0700

    Update 242. Valid Anagram.py

--- Neetcode/Arrays & Hashing/242. Valid Anagram.py
+++ Neetcode/Arrays & Hashing/242. Valid Anagram.py
@@ -95,3 +95,20 @@ class Solution:
             return True
         else:
             return False
+
+# My solution - OPTIMAL on 7/26/2025
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        count = [0] * 26
+
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        
+        for val in count:
+            if val != 0:
+                return False
+        return True
