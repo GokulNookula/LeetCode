0719cbc0c1f99209e03928c84a33053c9757cfe4
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-26 21:16:10 -0700

    Update 15. 3Sum.py

--- Neetcode/Two Pointers/15. 3Sum.py
+++ Neetcode/Two Pointers/15. 3Sum.py
@@ -208,3 +208,33 @@ class Solution:
                         left += 1
 
         return res
+
+# My solution - OPTIMAL on 7/26/2025
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        for i in range(len(nums)):
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while(left < right):
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    res.append([nums[i],nums[left],nums[right]])
+                    left += 1
+                    right -= 1
+
+                    while ((left < right) and nums[left] == nums[left - 1]):
+                        left += 1
+        return res
