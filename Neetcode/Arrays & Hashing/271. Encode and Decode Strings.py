5fa1111d6b09628ed168a1e191d9863776e43a6d
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-26 22:23:50 -0700

    Update 271. Encode and Decode Strings.py

--- Neetcode/Arrays & Hashing/271. Encode and Decode Strings.py
+++ Neetcode/Arrays & Hashing/271. Encode and Decode Strings.py
@@ -69,3 +69,31 @@ class Solution:
             res.append(s[i:j])
             i = j
         return res
+
+# My solution - Almost solved it on 7/26/2025
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        
+        encodedS = ''
+
+        for s in strs:
+            encodedS += str(len(s)) + "#" + s
+        
+        return encodedS
+
+    def decode(self, s: str) -> List[str]:
+
+        res = []
+        i = 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            i = j
+        return res
