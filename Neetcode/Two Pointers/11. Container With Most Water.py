da0814cd51f97c9815cdaec424f6384a749a2d9b
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-26 21:09:17 -0700

    Update 11. Container With Most Water.py

--- Neetcode/Two Pointers/11. Container With Most Water.py
+++ Neetcode/Two Pointers/11. Container With Most Water.py
@@ -109,3 +109,21 @@ class Solution:
             else:
                 right -= 1
         return maxArea
+
+# My solution - OPTIMAL on 7/26/2025
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right  = len(height) - 1
+        maxArea = float("-inf")
+
+        while (left < right):
+            area = (right - left) * min(height[left], height[right])
+
+            maxArea = max(maxArea, area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return maxArea
