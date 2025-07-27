1e685d92169694adaea48c05f8e7a4450079c4b9
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-26 21:05:38 -0700

    Update 42. Trapping Rain Water.py

--- Neetcode/Two Pointers/42. Trapping Rain Water.py
+++ Neetcode/Two Pointers/42. Trapping Rain Water.py
@@ -100,3 +100,25 @@ class Solution:
 
         return res
 
+# My solution - OPTIMAL on 7/26/2025
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        left = 0
+        right = len(height) - 1
+        maxLeft = height[left]
+        maxRight = height[right]
+        res = 0
+
+        while (left < right):
+
+            if maxLeft < maxRight:
+                left += 1
+                maxLeft = max(maxLeft, height[left])
+                res = res + maxLeft - height[left]
+            else:
+                right -= 1
+                maxRight = max(maxRight, height[right])
+                res = res + maxRight - height[right]
+        return res
+
