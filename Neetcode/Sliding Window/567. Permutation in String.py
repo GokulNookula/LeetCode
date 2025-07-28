f959a8e0729fc1a9d90589783d5b6cdcaa8e6837
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-28 00:10:12 -0700

    Update 567. Permutation in String.py

--- Neetcode/Sliding Window/567. Permutation in String.py
+++ Neetcode/Sliding Window/567. Permutation in String.py
@@ -122,3 +122,49 @@ class Solution:
             left += 1
 
         return matches == 26
+
+# My solution - Didnt solve it on 7/28/2025
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1) > len(s2):
+            return False
+        
+        s1Count = [0] * 26
+        s2Count = [0] * 26
+        left = 0
+        right = 0
+
+        for right in range(len(s1)):
+            s1Count[ord(s1[right]) - ord('a')] += 1
+            s2Count[ord(s2[right]) - ord('a')] += 1
+
+        matches = 0
+
+        for i in range(len(s1Count)):
+            if s1Count[i] == s2Count[i]:
+                matches += 1
+
+
+        for right in range(len(s1),len(s2)):
+
+            if matches == 26:
+                return True
+            
+            index = ord(s2[right]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+            
+            index = ord(s2[left]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+
+            left += 1
+
+        return matches == 26
