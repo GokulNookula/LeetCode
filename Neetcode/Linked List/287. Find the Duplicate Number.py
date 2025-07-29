e240a66557174a8a2d321a0ec35bb1afdec5e370
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-29 00:50:53 -0700

    Update 287. Find the Duplicate Number.py

--- Neetcode/Linked List/287. Find the Duplicate Number.py
+++ Neetcode/Linked List/287. Find the Duplicate Number.py
@@ -74,3 +74,26 @@ class Solution:
             if slow == slow2:
                 return slow
         return None
+
+# My solution - OPTIMAL on 7/29/2025
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = 0
+        fast = 0
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+        
+        slow2 = 0
+
+        while True:
+            slow2 = nums[slow2]
+            slow = nums[slow]
+
+            if slow == slow2:
+                break
+        return slow
