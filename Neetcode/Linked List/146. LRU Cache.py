fa7d574421e92235272f23211302c480444ca6a2
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-29 13:58:36 -0700

    Update 146. LRU Cache.py

--- Neetcode/Linked List/146. LRU Cache.py
+++ Neetcode/Linked List/146. LRU Cache.py
@@ -126,3 +126,64 @@ class LRUCache:
 
 ''' Explained: Time Complexity: O(1) for get() and put() Space Complexity: O(n)
 '''
+
+# My solution - Almost solved it on 7/29/2025
+class Node:
+    # Using a doubly linked list aka where
+    # we know the previous and next of each node
+    def __init__(self,key,val):
+        self.key = key
+        self.val = val
+
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+
+        self.left = Node(0,0)
+        self.right = Node(0,0)
+
+        self.left.next = self.right
+        self.right.prev = self.left
+    
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+
+        prev.next = nxt
+        nxt.prev = prev
+
+    def insert(self, node):
+        prev = self.right.prev
+        nxt = self.right
+
+        prev.next = node
+        nxt.prev = node
+
+        node.prev = prev
+        node.next = nxt
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+
+            return self.cache[key].val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key,value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+
+            self.remove(lru)
+
+            del self.cache[lru.key]
