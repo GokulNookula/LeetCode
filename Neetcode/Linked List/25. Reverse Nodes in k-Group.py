cec5cd59a01aa6d44314431663cd421411d6771f
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-29 15:05:21 -0700

    Update 25. Reverse Nodes in k-Group.py

--- Neetcode/Linked List/25. Reverse Nodes in k-Group.py
+++ Neetcode/Linked List/25. Reverse Nodes in k-Group.py
@@ -82,3 +82,44 @@ class Solution:
             left = right
 
         return dummy.next
+
+# My solution - Didnt solve it on 7/29/2025
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+
+        while True:
+            kth = self.getKth(groupPrev,k)
+
+            if not kth:
+                break
+            
+            groupNext = kth.next
+            prev = kth.next
+            curr = groupPrev.next
+
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+            
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+        return dummy.next
+
+    def getKth(self,node,k):
+        curr = node
+
+        while curr != None and k > 0:
+            curr = curr.next
+            k -= 1
+        
+        return curr
