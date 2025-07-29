3aa39964ebd8b64458bcf900e0232a48efa8539e
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-28 23:49:36 -0700

    Update 20. Valid Parentheses.py

--- Neetcode/Stack/20. Valid Parentheses.py
+++ Neetcode/Stack/20. Valid Parentheses.py
@@ -84,3 +84,19 @@ class Solution:
                 else:
                     return False
         return not stack
+
+# My solution - OPTIMAL on 7/28/2025
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mydict = {")": "(", "}": "{", "]": "["}
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] not in mydict:
+                stack.append(s[i])
+            else:
+                if stack and mydict[s[i]] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
