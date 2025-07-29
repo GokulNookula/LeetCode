e0fc6ed03f84a66dd9ec605c7ae46cada21c2257
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-28 23:29:06 -0700

    Update 150. Evaluate Reverse Polish Notation.py

--- Neetcode/Stack/150. Evaluate Reverse Polish Notation.py
+++ Neetcode/Stack/150. Evaluate Reverse Polish Notation.py
@@ -57,3 +57,26 @@ class Solution:
                 stack.append(int(tokens[i]))
         return stack[0]
         
+# My solution - OPTIMAL on 7/28/2025
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        for i in range(len(tokens)):
+            if tokens[i] == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif tokens[i] == "-":
+                a = stack.pop()
+                b = stack.pop()
+
+                stack.append(b - a)
+            elif tokens[i] == "*":
+                stack.append((stack.pop() * stack.pop()))
+            elif tokens[i] == "/":
+                a = stack.pop()
+                b = stack.pop()
+
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(tokens[i]))
+        return stack[0]      
