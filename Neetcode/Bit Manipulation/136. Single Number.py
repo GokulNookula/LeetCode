d974b5b1cae65dde8aceda9f233085b757cec112
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-07-29 17:53:02 -0700

    Update 136. Single Number.py

--- Neetcode/Bit Manipulation/136. Single Number.py
+++ Neetcode/Bit Manipulation/136. Single Number.py
@@ -1,11 +1,18 @@
 # OPTIMAL Solution
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
-        index = 0
+        # Initalizing a value to store as 0
+        # because XORing anything with 0 is always
+        # going to return the number itself
+        res = 0
 
+        # Iterating through the entire nums array
         for n in nums:
-            index = index ^ n
-        return index
+            # We basically XOR each number until the
+            # end however if we have even number of 1's
+            # or 0's those cancel out due to XOR logic
+            res = res ^ n
+        # Returning the single number that was found
+        return res
 '''Explained: Time Complexity: O(n) Space Complexity: O(1)
 '''
