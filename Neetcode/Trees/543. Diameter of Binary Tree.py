e3a2b8afb16e339efd9e70ab3454c563fbffdaf7
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-09-02 15:47:58 -0700

    Update 543. Diameter of Binary Tree.py

--- Neetcode/Trees/543. Diameter of Binary Tree.py
+++ Neetcode/Trees/543. Diameter of Binary Tree.py
@@ -27,3 +27,24 @@ class Solution:
 
 '''Explained: Time Complexity: O(n) Space Complexity: O(n)
 '''
+
+# My solution - Didn't solve it on 9/2/2025
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        maxDiamater = 0
+
+        def dfs(root):
+            nonlocal maxDiamater
+
+            if root is None:
+                return 0
+
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            maxDiamater = max(maxDiamater, left + right)
+
+            return 1 + max(left,right)
+        dfs(root)
+        return maxDiamater
