6e8e8c4f2934e94d7bab63fc3be0ef8479c6db60
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-09-13 23:16:56 -0700

    Update 146. LRU Cache.py

--- Neetcode/Linked List/146. LRU Cache.py
+++ Neetcode/Linked List/146. LRU Cache.py
@@ -190,3 +190,65 @@ class LRUCache:
 
 # My solution - Didn't solve it on 9/13/2025
 
+class Node:
+    def __init__(self,key,val):
+        self.key = key
+        self.val = val
+
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+
+        self.left = Node(0,0)
+        self.right = Node(0,0)
+
+        self.left.next = self.right
+        self.right.prev = self.left
+    
+    def insert(self,node):
+        prev = self.right.prev
+        nxt = self.right
+
+        prev.next = node
+        nxt.prev = node
+
+        node.next = nxt
+        node.prev = prev
+
+    def remove(self,node):
+        prev = node.prev
+        nxt = node.next
+
+        prev.next = nxt
+        nxt.prev = prev
+        
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key,value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+
+            self.remove(lru)
+            del self.cache[lru.key]
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
