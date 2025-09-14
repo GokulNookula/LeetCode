96cce13e03221d2e36eaa99082bb916762a60883
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-09-14 00:14:58 -0700

    Update 141. Linked List Cycle.py

--- Neetcode/Linked List/141. Linked List Cycle.py
+++ Neetcode/Linked List/141. Linked List Cycle.py
@@ -90,3 +90,17 @@ class Solution:
                 return True
 
         return False
+
+# My solution - OPTIMAL on 9/14/2025
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        return False
