f751435f1ea1d8c48162459fe448d76f4b3f3002
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-09-14 19:22:45 -0700

    Update 155. Min Stack.py

--- Neetcode/Stack/155. Min Stack.py
+++ Neetcode/Stack/155. Min Stack.py
@@ -85,3 +85,30 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.minStack[-1]
+
+# My solution - OPTIMAL on 9/14/2025
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+        
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minStack and self.minStack[-1] > val:
+            self.minStack.append(val)
+        elif len(self.minStack) != 0:
+            self.minStack.append(self.minStack[-1])
+        else:
+            self.minStack.append(val)
+        
+
+    def pop(self) -> None:
+        del self.stack[-1]
+        del self.minStack[-1]
+
+    def top(self) -> int:
+        return self.stack[-1]
+    
+    def getMin(self) -> int:
+        return self.minStack[-1]
