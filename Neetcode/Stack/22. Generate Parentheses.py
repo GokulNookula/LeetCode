2a0acff2818a5673cf36b5596ba413740c3641bb
Gokul Nookula <111548159+GokulNookula@users.noreply.github.com>
2025-09-14 19:45:07 -0700

    Update 22. Generate Parentheses.py

--- Neetcode/Stack/22. Generate Parentheses.py
+++ Neetcode/Stack/22. Generate Parentheses.py
@@ -72,4 +72,25 @@ class Solution:
                 backTrack(openN,closeN + 1)
                 stack.pop()
         backTrack(0,0)
-        return res 
+        return res
+
+# My solution - OPTIMAL on 9/14/2025
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+
+        def backTrack(openN, closeN):
+            if openN == closeN == n:
+                res.append("".join(stack))
+                return
+            if openN < n:
+                stack.append("(")
+                backTrack(openN + 1, closeN)
+                stack.pop()
+            if closeN < openN:
+                stack.append(")")
+                backTrack(openN, closeN + 1)
+                stack.pop()
+        backTrack(0,0)
+        return res
